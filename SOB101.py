5fe3e1f3d30d689b730b01bb9dfa916949bea4f5
Myu <ms3433@live.mdx.ac.uk>
2023-11-26 04:00:03 +0000

    alterntive for SOB 101

--- SOB101.py
+++ SOB101.py
@@ -11,6 +11,19 @@ for i in range(r, 0, -1):
 for i in range(0, r+1, 1): 
 
     print(" "*(r-i), "*")
+
+# 2
+
+star_list = ["*"] * 10 # ['*','*',....]
+r = len(star_list)-1 # 10
+
+i = 0
+while i < r:
+    if i < 5:
+        print(" "*(i), star_list[i] )
+    else:
+        print(" "*(r-i-1), star_list[i])
+    i+=1
     
 # 3
 
