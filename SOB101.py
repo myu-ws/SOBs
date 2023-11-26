b27883611dcbe50e74b59a34eeb875695dffc67d
Myu <ms3433@live.mdx.ac.uk>
2023-11-26 04:17:50 +0000

    changes to number 3

--- SOB101.py
+++ SOB101.py
@@ -2,7 +2,7 @@
 
 print("Program to print arrow pattern: \n") 
 
-r = 5
+r = 4
 
 for i in range(r, 0, -1): 
 
@@ -13,6 +13,8 @@ for i in range(0, r+1, 1):
     print(" "*(r-i), "*")
 
 # 2
+
+print("\n")
 
 star_list = ["*"] * 10 # ['*','*',....]
 r = len(star_list)-1 # 10
@@ -27,19 +29,20 @@ while i < r:
     
 # 3
 
-r = 5
+print("\n")
 
 def zigzag():
-    for i in range(r, 0, -1): 
 
-        print(" "*(r-i), "*")
-    
-    for i in range(0, r+1, 1): 
+    star_list = ["*"] * 10 # ['*','*',....]
+    r = len(star_list)-1 # 10
 
-        print(" "*(r-i), "*")
+    i = 0
+    while i < r:
+        if i < 5:
+            print(" "*(i), star_list[i] )
+        else:
+            print(" "*(r-i-1), star_list[i])
+        i+=1
 
-for n in range(4):
+for n in range(3):
     zigzag()
-
-
-
